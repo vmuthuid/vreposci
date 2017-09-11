70a6d19bf8703bfc2cf9ac7dd55aad7abbece2d0
Velmurugan Muthu <velmurugan.muthu@hmcts.net>
2017-09-11 16:24:02 +0100

    first commit

---
+++ mm-parent/mm-lib/src/main/java/com/vel/tries/mm/lib/Util.java
@@ -0,0 +1,14 @@
+package com.vel.tries.mm.lib;
+
+public class Util {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static int add(int a, int b) {
+		return a+b;
+	}
+
+}
---
+++ mm-parent/mm-lib/src/test/java/com/vel/tries/mm/lib/UtilTest.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package com.vel.tries.mm.lib;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author vmuthu
+ *
+ */
+public class UtilTest {
+
+	Util util = new Util();
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddPass() {
+		int result = util.add(2,3);
+		assertEquals(5, result);
+	}
+
+	@Test
+	public void testAddMismatch() {
+		int result = util.add(3,3);
+		assertNotSame(result, 5);
+	}
+
+}
