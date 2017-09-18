87902c13f6b20defbfa5b63ab56509449352e48c
Velmurugan Muthu <velmurugan.muthu@hmcts.net>
2017-09-18 17:28:47 +0100

    added the java file to web application

---
+++ mm-parent/mm-web1/src/main/java/com/vel/tries/mm/web1/Web1Util.java
@@ -0,0 +1,14 @@
+package com.vel.tries.mm.web1;
+
+public class Web1Util {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static int add(int a, int b) {
+		return a+b;
+	}
+
+}
