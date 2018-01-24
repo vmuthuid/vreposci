1e9505d594f32d550007d6034611387e23e96515
vmuthuid <31856066+vmuthuid@users.noreply.github.com>
2018-01-24 18:26:36 +0000

    Create JasperreportUse.java

---
+++ mm-parent/mm-web2/src/main/java/com/vel/tries/mm/web2/JasperreportUse.java
@@ -0,0 +1,13 @@
+package com.vel.tries.mm.web2;
+
+import org.apache.jasper.JasperException;
+
+public class JasperreportUse {
+	
+
+	public static void main(String[] args) {
+		new JasperException("");
+
+	}
+
+}
